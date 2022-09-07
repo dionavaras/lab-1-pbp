88228484ec42671cb8d9426e437ac18f0ced5cd7
dionavarastika <diona.varastika@ui.ac.id>
2022-09-07 11:45:49 +0700

    lab 1 done

---
+++ wishlist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Diona'
+}
+    return render(request, "wishlist.html", context)
+
+
+
